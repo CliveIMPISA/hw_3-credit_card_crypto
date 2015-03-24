2b584acae12d2a309e9d9f9fe92a2448df607a3b
Clive <CliveImpisa>
2015-03-24 21:34:01 +0800

    Rubocop formatted a bit

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,8 +1,10 @@
+# Luhn Validator modulerese
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
+
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
     nums_a = nums_a.reverse
@@ -11,24 +13,18 @@ module LuhnValidator
     counter = 1
     valid = true
     account_number.each do |p|
-
       if counter.even?
         sum += p
       else
-        if (p*2) > 9
-          sum += (p*2).to_s.split(//).map { |x| x.to_i }.inject(:+)
-          puts "this is even #{sum}, #{p*2}"
+        if (p * 2) > 9
+          sum += (p * 2).to_s.chars.map(&:to_i).inject(:+)
         else
-          sum += (p*2)
-          puts "this is even #{sum}, #{p*2}"
+          sum += (p * 2)
         end
       end
-      counter+=1
+      counter += 1
     end
-    puts sum
-    sum = sum * 9
-    puts sum
-    puts sum.to_s.chars.map {|f| f}.last
+    sum *= 9
 
     if sum % 10 == nums_a.first
       valid = true
