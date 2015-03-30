db280d782974e53d528523012552fa553cff7f16
Clive <CliveImpisa>
2015-03-30 10:06:30 +0800

    Modified luhn's algorithm

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -6,17 +6,18 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
 
   def validate_checksum
+
     nums_a = number.to_s.chars.map(&:to_i)
     nums_a = nums_a.reverse
     account_number = nums_a.drop(1)
     sum = 0
     counter = 1
-    account_number.each do |p|
+    account_number.map do |p|
       if counter.even?
         sum += p
       else
         if (p * 2) > 9
-          sum += (p * 2).to_s.chars.map(&:to_i).inject(:+)
+          sum += (p * 2)-9
         else
           sum += (p * 2)
         end
