7ee839f3e92e6af23edc320d7410deedcdbb8a58
Clive <CliveImpisa>
2015-04-02 15:36:34 +0800

    completed AES_cipher and Double tranpositions Cipher

--- aes_cipher.rb
+++ aes_cipher.rb
@@ -7,9 +7,22 @@ module AesCipher
     #       and ciphertext is the output of AES encryption
     # NOTE: Use hexadecimal strings for output so that it is screen printable
     #       Use cipher block chaining mode only!
+    cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    cipher.encrypt
+    cipher.key = key
+    iv = cipher.random_iv
+    ciphertext = cipher.update(plaintext) + cipher.final
+    output = [ iv.unpack("H*"),ciphertext.unpack("H*")]
+    output.to_json
   end
 
   def self.encrypt(aes_crypt, key)
     # TODO: decrypt from JSON output (aes_crypt) of encrypt method above
+    decrypter = OpenSSL::Cipher::AES.new(128, :CBC)
+    decrypter.decrypt
+    decrypter.key = key
+    decrypter.iv = JSON.parse(aes_crypt)[0].pack("H*")
+    ciphertext = JSON.parse(aes_crypt)[1].pack("H*")
+    plain = decrypter.update(ciphertext) + decrypter.final
   end
 end
--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -7,9 +7,47 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
+    doc_length =  document.to_s.length
+    row = Math.sqrt(doc_length).round
+    col = 0
+    if row == Math.sqrt(doc_length).to_i
+      col = row
+    else
+      col = (doc_length / row.round) + 1
+    end
+    doc = document.to_s.chars.to_a.each_slice(col).to_a
+    doc = doc.shuffle(random: Random.new(key))
+    doc.map! {|p| p.shuffle(random: Random.new(key))}
+    doc.join
   end
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
+    ciphertext = ciphertext.to_s.chars.to_a
+    doc_length =  ciphertext.length
+    row = Math.sqrt(doc_length).round
+    decipher_arr = (1..doc_length).to_a
+
+
+
+    if row == Math.sqrt(doc_length).to_i
+      col = row
+    else
+      col = (doc_length / row) + 1
+    end
+    decipher_arr =  decipher_arr.each_slice(col).to_a
+    decipher_arr = decipher_arr.shuffle(random: Random.new(key))
+
+    decipher_arr.map! { |p| p.shuffle(random: Random.new(key))}
+
+    decipher_arr = decipher_arr.flatten
+
+    counter = 1
+    plaintext = []
+    decipher_arr.each do |m|
+      plaintext << ciphertext[decipher_arr.index(counter)]
+      counter += 1
+    end
+    plaintext.join
   end
 end
