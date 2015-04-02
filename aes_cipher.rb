5852ff3f437ba99b2872034083271e94bc035df0
Clive <CliveImpisa>
2015-04-02 22:48:26 +0800

    add error checking block for short passwords

--- aes_cipher.rb
+++ aes_cipher.rb
@@ -3,18 +3,18 @@ require 'json'
 
 module AesCipher
   def self.encrypt(document, key)
-    # TODO: Return JSON string of array: [iv, ciphertext]
-    #       where iv is the random intialization vector used in AES
-    #       and ciphertext is the output of AES encryption
-    # NOTE: Use hexadecimal strings for output so that it is screen printable
-    #       Use cipher block chaining mode only!
-    cipher = OpenSSL::Cipher::AES.new(128, :CBC)
-    cipher.encrypt
-    cipher.key = key.to_s
-    iv = cipher.random_iv
-    ciphertext = cipher.update(document) + cipher.final
-    output = [ iv.unpack("H*"),ciphertext.unpack("H*")]
-    output.to_json
+    begin
+      cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+      cipher.encrypt
+      cipher.key = key.to_s
+      iv = cipher.random_iv
+      ciphertext = cipher.update(document) + cipher.final
+      output = [ iv.unpack("H*"),ciphertext.unpack("H*")]
+    rescue
+      "Passphrase too short! Should be at least 16 characters long"
+    else
+      output.to_json
+    end
   end
 
   def self.decrypt(aes_crypt, key)
@@ -27,7 +27,7 @@ module AesCipher
       ciphertext = JSON.parse(aes_crypt)[1].pack("H*")
       plain = decrypter.update(ciphertext) + decrypter.final
     rescue
-      "Error"
+      "Error! Incorrect PassPhrase Enter. Please Try Again"
     else
       plain
     end
